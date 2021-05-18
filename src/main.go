eabf29a47d3c0721d4425ef59afc89350ce4976f
KGB33 <github@bassingthwaite.org>
2021-05-18 06:39:01 -0600

    created basic webserver

--- src/main.go
+++ src/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	handleRequests()
+}
+
+func handleRequests() {
+	http.HandleFunc("/", index)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the homepage")
+	log.Printf("Homepage index hit by %v\n", r)
+}
