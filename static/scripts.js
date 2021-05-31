b0c43605f33c496ec0ed5617c7e50aed8396ace1
KGB33 <github@bassingthwaite.org>
2021-05-31 08:46:38 -0600

    added create URL dialog box & fixed get URL button

--- static/scripts.js
+++ static/scripts.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8080"
+const BASE_URL = "http://127.0.0.1:8080/api/v1"
 
 const app = new Vue({
 	el: "#main",
@@ -9,23 +9,22 @@ const app = new Vue({
 	methods: {
 		fetchAllUrls() {
 			this.resultAvailable = false;
-			console.log("Hello fetchAllUrls");
 
-			fetch(BASE_URL, {
+			fetch(BASE_URL + "/r", {
 				"method": "GET",
 				"headers": {
 					//"api-key": "key-go-here",
-					"Access-Control-Allow-Origin": "127.0.0.1",
 				}
 
 				}).then(response => {
-					if(response.ok) {return response.json}
+					if(response.ok) {return response.json()}
 					else {
 						alert("Server returned: " + response.status + " : " + response.statusText);
 					}
 				})
-				.then(response => {
-					this.result = response.body;
+				.then(data => {
+					console.log(data);
+					this.result = data;
 					this.resultAvailable = true;
 				})
 				.catch(err => {
@@ -33,4 +32,36 @@ const app = new Vue({
 				});
 			}
 		}
+})
+
+const createUrlBox = new Vue({
+	el: "#createBox",
+	data: {
+		shortUrl: "foo",
+		destUrl: "bar",
+		created: false
+	},
+	methods: {
+		createUrl() {
+			fetch(BASE_URL + "/c", {
+				"method": "POST",
+				"body": `{"ShortUrl": "${this.shortUrl}", "DestUrl": "${this.destUrl}"}`
+			}).then(response => {
+				return response.json()
+			}).then(data => {
+				if(data.Error) {
+					alert("Server returned: " + data.Error);
+				} else {
+					this.ShortUrl = data.ShortUrl;
+					this.DestUrl = data.DestUrl;
+					this.created = true
+				}
+			})
+			.catch(err => {
+				console.log(err);
+			});
+
+		}
+	}
+
 })
